5c0c54fb2b1b5cfa909d47cad06b6de1f58c5705
Anna <anna@MacBook-Air-von-Nutzerin1.local>
2025-06-22 23:19:09 +0200

    added function to generate html from template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,30 +5,43 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-def main():
-    animals_data = load_data('animals_data.json')
+def generate_string_from_json(animals_data):
+    animal_info = ""
     for animal in animals_data:
         try:
             name = animal['name']
-            print(f"Name: {name}")
+            animal_info += f"Name: {name}\n"
         except KeyError:
             pass
         try:
             diet = animal['taxonomy']['order']
-            print(f"Diet: {diet}")
+            animal_info += f"Diet: {diet}\n"
         except KeyError:
             pass
         try:
             location = ", ".join(animal['locations'])
-            print(f"Location: {location}")
+            animal_info += f"Location: {location}\n"
         except KeyError:
             pass
         try:
             type = animal['characteristics']['type']
-            print(f"Type: {type}")
+            animal_info += f"Type: {type}\n"
         except KeyError:
             pass
-        print("")
+        animal_info += "\n"
+    return(animal_info)
+
+def main():
+    animals_data = load_data('animals_data.json')
+    website_string = generate_string_from_json(animals_data)
+
+    with open ("animals_template.html", "r") as origin_file:
+        html_skeleton = origin_file.read()
+    with open("animals.html", "w") as newfile:
+        newfile.write(html_skeleton.replace("__REPLACE_ANIMALS_INFO__", website_string))
+
+
+
 
 if __name__ == "__main__":
     main()
