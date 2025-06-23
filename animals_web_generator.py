07979abfb015a00f30bae0b84fb8e89ce9d8f1a0
Anna <anna@MacBook-Air-von-Nutzerin1.local>
2025-06-23 18:37:57 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,29 +6,35 @@ def load_data(file_path):
     return json.load(handle)
 
 def generate_string_from_json(animals_data):
+    """
+    take json data and transform into html code: information blocks for every animal in the database
+    :param animals_data:
+    :return: html-string "animal_info"
+    """
     animal_info = ""
     for animal in animals_data:
+        animal_info += "<li class='cards__item'>"
         try:
             name = animal['name']
-            animal_info += f"Name: {name}\n"
+            animal_info += f"Name: {name}<br/>\n"
         except KeyError:
             pass
         try:
             diet = animal['taxonomy']['order']
-            animal_info += f"Diet: {diet}\n"
+            animal_info += f"Diet: {diet}<br/>\n"
         except KeyError:
             pass
         try:
             location = ", ".join(animal['locations'])
-            animal_info += f"Location: {location}\n"
+            animal_info += f"Location: {location}<br/>\n"
         except KeyError:
             pass
         try:
             type = animal['characteristics']['type']
-            animal_info += f"Type: {type}\n"
+            animal_info += f"Type: {type}<br/>\n"
         except KeyError:
             pass
-        animal_info += "\n"
+        animal_info += "</li>"
     return(animal_info)
 
 def main():
