2e174b049a7dbd1d802d472ddd63752e0c9ab3c6
Anna <anna@MacBook-Air-von-Nutzerin1.local>
2025-06-23 22:52:58 +0200

    cleaned up code

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,53 +1,63 @@
 import json
 
+
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-def generate_string_from_json(animals_data):
+
+def generate_html_string_from_json(animals_data):
     """
     take json data and transform into html code: information blocks for every animal in the database
     :param animals_data:
     :return: html-string "animal_info"
     """
+    website_string = ""
+    for animal_obj in animals_data:
+        website_string += serialize_animal(animal_obj)
+    return website_string
+
+
+def serialize_animal(animal_obj):
+    """
+    take one dictionary from the original list from json and turn it into html
+    """
     animal_info = ""
-    for animal in animals_data:
-        animal_info += "<li class='cards__item'>"
-        try:
-            name = animal['name']
-            animal_info += f"<div class='card__title'> {name}</div>"
-        except KeyError:
-            pass
-        animal_info += "<p class='card__text'>"
-        try:
-            diet = animal['taxonomy']['order']
-            animal_info += f"<strong>Diet:</strong> {diet}<br/>\n"
-        except KeyError:
-            pass
-        try:
-            location = ", ".join(animal['locations'])
-            animal_info += f"<strong>Location:</strong> {location}<br/>\n"
-        except KeyError:
-            pass
-        try:
-            type = animal['characteristics']['type']
-            animal_info += f"<strong>Type:</strong> {type}<br/>\n"
-        except KeyError:
-            pass
-        animal_info += "</p></li>"
+    animal_info += "<li class='cards__item'>"
+    try:
+        name = animal_obj['name']
+        animal_info += f"<div class='card__title'> {name}</div>"
+    except KeyError:
+        pass
+    animal_info += "<p class='card__text'>"
+    try:
+        diet = animal_obj['taxonomy']['order']
+        animal_info += f"<strong>Diet:</strong> {diet}<br/>\n"
+    except KeyError:
+        pass
+    try:
+        location = ", ".join(animal_obj['locations'])
+        animal_info += f"<strong>Location:</strong> {location}<br/>\n"
+    except KeyError:
+        pass
+    try:
+        type = animal_obj['characteristics']['type']
+        animal_info += f"<strong>Type:</strong> {type}<br/>\n"
+    except KeyError:
+        pass
+    animal_info += "</p></li>"
     return(animal_info)
+
 
 def main():
     animals_data = load_data('animals_data.json')
-    website_string = generate_string_from_json(animals_data)
+    website_string = generate_html_string_from_json(animals_data)
 
     with open ("animals_template.html", "r") as origin_file:
         html_skeleton = origin_file.read()
     with open("animals.html", "w") as newfile:
         newfile.write(html_skeleton.replace("__REPLACE_ANIMALS_INFO__", website_string))
-
-
 
 
 if __name__ == "__main__":
